7fdb2599141620cd59bec63287389aa5749e44a6
Nick Sanford <nicholascsanford@gmail.com>
2020-04-26 16:36:50 -0400

    wip

--- mix.exs
+++ mix.exs
@@ -7,14 +7,21 @@ defmodule MinecraftAdmin.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: [
+        minecraft_admin: [
+          include_executables_for: [:unix],
+          applications: [runtime_tools: :permanent],
+          strip_beams: false
+        ]
+      ]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :runtime_tools],
       mod: {MinecraftAdmin.Application, []}
     ]
   end
@@ -22,6 +29,7 @@ defmodule MinecraftAdmin.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:observer_cli, "~> 1.5"},
       {:nostrum, "~> 0.4"}
     ]
   end
